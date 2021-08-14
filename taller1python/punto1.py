0c85c09b513263a66a34e8bae2cfa9674faf6150
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-14 12:52:05 -0500

    Create punto1.py

---
+++ taller1python/punto1.py
@@ -0,0 +1,8 @@
+print("Ingrese la edad numero1 ")
+a= int(input())
+print("Ingrese la edad numero2 ")
+b= int(input())
+print("Ingrese la edad numero3 ")
+c= int(input())
+promedio= (a+b+c)/3
+print("el promedio de edad de tres personas es de %d" %promedio)
