a463affa529be0e6abce9789ce340a4adced33da
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-14 13:23:34 -0500

    Create punto4.py

---
+++ taller1python/punto4.py
@@ -0,0 +1,10 @@
+"""
+entrada
+total_compra-->float-->a
+Salida
+precio_total-->float-->pt
+"""
+a= float(input("escriba el total de su compra"))
+vd=(a*0.15)
+pt=a-vd
+print("el valor total de su compra es: ",str(pt))
