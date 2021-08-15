f0f907122589e5e2750fc4612c56569c9d4de2ec
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-15 12:33:50 -0500

    Create punto9.py

---
+++ taller1python/punto9.py
@@ -0,0 +1,14 @@
+"""
+Entradas
+Longitud del triangulo-->float-->a,b,c
+Salida
+sempiperimetro-->float-->d=(a+b+c)/2
+Area-->float-->area=math.sqrt(d*(d-a)*(d-b)*(d-c))
+"""
+import math
+print("Escriba las longitudes de los lados del triangulo: ")
+a, b, c = map(float, input().split())
+d=(a+b+c)/2
+area=math.sqrt(d*(d-a)*(d-b)*(d-c))
+print("El area del triangulo es: ", area)
+ 
