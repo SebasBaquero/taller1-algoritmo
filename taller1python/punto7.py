39cb8ae0ddf721f5b42b00d3bc9c0c2d540d5c38
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-15 00:33:24 -0500

    Create punto7.py

---
+++ taller1python/punto7.py
@@ -0,0 +1,12 @@
+"""
+Entrada
+Metros-->float-->metros
+Salida
+Pulgadas-->float-->pulgadas
+Pies-->float-->pies
+"""
+metros=float(input("Digite los metros que quiera convertir: "))
+pulgadas=metros*39.37
+pies=metros*3.281
+print("La conversion en pies es:", pies)
+print("La conversion en pulgadas es:",pulgadas)
