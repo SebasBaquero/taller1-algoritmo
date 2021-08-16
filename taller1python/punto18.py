80b9f1bf8788689f1f28b900048ddc6a7106645a
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-15 20:43:06 -0500

    Update punto18.py

--- taller1python/punto18.py
+++ taller1python/punto18.py
@@ -6,7 +6,7 @@ tiempo-->float-->t
 salida
 intereses-->str-->inte
 """
-a=float(input ("Escreiba el valor del pestramo: " ))
+a=float(input ("Escriba el valor del pestramo: " ))
 b=float(input ("Escriba el valor de los intereses: "))
 t=4
 inte=(a*t*b)/100
