0473c5d6c59c50b14abb4a9f216ee7ea33ea92d9
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-15 20:40:42 -0500

    Create punto18.py

---
+++ taller1python/punto18.py
@@ -0,0 +1,13 @@
+"""
+entrada
+valor_prestamo-->float-->a
+valor_intereses-->float-->b
+tiempo-->float-->t
+salida
+intereses-->str-->inte
+"""
+a=float(input ("Escreiba el valor del pestramo: " ))
+b=float(input ("Escriba el valor de los intereses: "))
+t=4
+inte=(a*t*b)/100
+print("el interes en cuatro años es del " +str (inte))
