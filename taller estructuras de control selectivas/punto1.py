b0b51d4cc9dfac2cee93361b623ecfcdc1f2e1f6
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-21 18:41:52 -0500

    Update punto1.py

--- taller estructuras de control selectivas/punto1.py
+++ taller estructuras de control selectivas/punto1.py
@@ -1,1 +1,7 @@
-
+capital= float(input("Ingrese el capital a invertir: "))
+tasa= float(input("Ingrese la tasa de interes: "))
+interes= tasa*capital/100
+print("Interes $", interes)
+if interes > 100000:
+  print("los intereses superan los $100.000 ")
+print("el saldo final ya con los intereses es: $", capital+interes)
