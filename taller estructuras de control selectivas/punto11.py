a4a6f68939a0a02dbdd847eeb1041196cc8b0fca
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-21 19:11:39 -0500

    Create punto11.py

---
+++ taller estructuras de control selectivas/punto11.py
@@ -0,0 +1,11 @@
+t= float(input("Ingrese la temperatura: "))
+if (t>85):
+  print("Debes de practicar natacion")
+elif(t>70 and t<=85):
+  print("Debas de practicar Tenis")
+elif(t>32 and t<=70):
+  print("Debas de practicar Golf")
+elif(t>10 and t<=32):
+  print("Debas de practicar Esqui")
+elif(t<=10):
+  print("Debes practicar Marcha")
