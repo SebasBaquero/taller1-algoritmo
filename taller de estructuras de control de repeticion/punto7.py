ee8efd7e74a1e04de54f830843dad80bf3f396d5
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-08-25 22:30:28 -0500

    Update punto7.py

--- taller de estructuras de control de repeticion/punto7.py
+++ taller de estructuras de control de repeticion/punto7.py
@@ -1,1 +1,10 @@
-
+"""
+Entradas
+incremento de valor de EXP de monstruos-->x
+el valor EXP del monstruo-->m
+"""
+while True:
+    x, m = input().split()
+    if(x == '0' and m == '0'):
+        break
+    print(int(x)*int(m))
