ff2c3a3818eb0bf89cb66b36f4927be3b0cfac00
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-08 14:15:33 -0500

    Update punto1.py

--- estructura de control for/punto1.py
+++ estructura de control for/punto1.py
@@ -1,1 +1,18 @@
+#1Cuente e Imprima cuantas ciudades inician con la letra M
 
+archivo = open("paises.txt", "r")
+lista = []
+ciudad = []
+for i in archivo:
+  a= i.index(":")
+  for r in range(a + 2, len(i)):
+    lista.append(i[r])
+  a= "".join(lista)
+  ciudad.append(a)
+  lista= []
+for i in ciudad:
+	if(i[0] == "M"):
+		print(i)
+		lista.append(i)
+print(len(lista))
+archivo.close()
