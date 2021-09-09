daf3ffb876aa0155e3d859586c232416d03bf248
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-08 23:37:37 -0500

    Update punto11.py

--- estructura de control for/punto11.py
+++ estructura de control for/punto11.py
@@ -1,12 +1,18 @@
 #Agregue un país que no esté en la lista
 archivo= open("paises.txt", "r")
-lista=[]
-lista.append("Vegetta\n")
+c= 0
+lista= []
 for i in archivo:
-  a=i.index(":")
-  for r in range(0,a):
-    lista.append(i[r])
-  a= "".join(pais)
-  print(a)
-  lista= []
+  lista.append(i)
+  a= " ".join(lista)
+  c= c+1 
+  if(a=="Paises\n"):
+    break
+b= a.index(":")
+lista1= [] 
+for i in range(0,len(a)):
+      lista1.append(a[i])
+lista1.insert(0,"Wakanda: Forever\n")
+u="".join(lista1)
+print(u)
 archivo.close()
