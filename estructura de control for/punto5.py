6da3a2ca44b5449473401f876a9ee79d9967f6dc
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-08 22:38:14 -0500

    Create punto5.py

---
+++ estructura de control for/punto5.py
@@ -0,0 +1,11 @@
+#Busque e imprima el pais de Venezuela y su capital
+archivo= open("paises.txt", "r")
+lista= []
+for i in archivo:
+  lista.append(i)
+  a= " ".join(lista)
+  if(a == "Venezuela: Caracas \n"):
+    break
+  lista = []
+print(a)
+archivo.close()
