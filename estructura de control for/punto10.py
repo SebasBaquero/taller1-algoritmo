8dc1c4e5fa90f0d49652af5d5842da0c5cefc0f8
Sebastian Baquero <87994668+SebasBaquero@users.noreply.github.com>
2021-09-08 23:58:53 -0500

    Update punto10.py

--- estructura de control for/punto10.py
+++ estructura de control for/punto10.py
@@ -1,22 +1,16 @@
 #El alcalde de Antananarivo contrato a algunos alumnos de la Universidad Ean para corregir el archivo de países.txt, ya que la capital de Madagascar NO es rey julien es Antananarivo, espero que el alcalde se vaya contento por su trabajo. Utilice un For para cambiar ese Dato
-archivo= open("paises.txt", "r")
-lista=[]
+archivo = open('paises.txt', 'r')
+lista= []
 for i in archivo:
   lista.append(i)
-  a= " ".join(lista)
+  a= "".join(lista)
   if(a=="Madagascar: rey julien\n"):
     break
-    for i in archivo:
-      lista= i.index(":")
-    for r in range(a+2,len(i)):
-      lista.append(i[r])
-    a= "".join(lista)
-    lista.append(a)
-    for i in pais:
-      if(i=="rey julien"):
-        lista.append(i)
-        lista.pop(i)
-        lista.append("Antananarivo")
-    lista= []
-    print(lista)
+lista1= []
+for i in archivo:
+  a= i.index(":")
+for i in range(0,len(i)):
+  lista1.remove("rey julien")
+  lista1.insert("Madagascar: Antananarivo")
+  print(lista1)
 archivo.close()
